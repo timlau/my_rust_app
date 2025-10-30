94a6c656f8f37fe757300525b4a684db631a8ae4
Tim Lauridsen <tla@rasmil.dk>
2025-10-30 13:51:12 +0100

    Change apllication window title

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() {
     gettextrs::bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR).expect("Unable to bind the text domain");
     gettextrs::textdomain(GETTEXT_PACKAGE).expect("Unable to switch to the text domain");
 
-    glib::set_application_name(&gettext("My First App"));
+    glib::set_application_name(&gettext("My Rust App"));
 
     let res = gio::Resource::load(RESOURCES_FILE).expect("Could not load gresource file");
     gio::resources_register(&res);
