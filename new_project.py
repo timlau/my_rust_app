26792732fe484b71b0a2a3f8357143cfe0c60abf
Tim Lauridsen <tla@rasmil.dk>
2025-11-02 07:56:51 +0100

    update new_project rool to handle Makefile and .spec

--- new_project.py
+++ new_project.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 
 class ProjectCreator:
-    dirs = [
+    source_dirs = [
         "src",
         "data",
         ".github",
@@ -15,7 +15,7 @@ class ProjectCreator:
         "po",
         ".vscode",
     ]
-    files = [
+    source_files = [
         ".gitignore",
         ".editorconfig",
         "meson.build",
@@ -24,6 +24,9 @@ class ProjectCreator:
         "Cargo.lock",
         "local.sh",
         "README.md",
+        "Makefile",
+        "my_rust_app.spec",
+        "LICENSE",
     ]
     source_id = "org.mydomain.MyRustApp"
     source_name = "my_rust_app"
@@ -44,7 +47,7 @@ class ProjectCreator:
         )
 
     def copy_directories(self):
-        for dir_name in self.dirs:
+        for dir_name in self.source_dirs:
             print(f" --> Copying directory : {dir_name}")
             shutil.copytree(
                 self.source_path / dir_name,
@@ -53,19 +56,16 @@ class ProjectCreator:
             )
 
     def copy_files(self):
-        for file_name in self.files:
+        for file_name in self.source_files:
             print(f" --> Copying file : {file_name}")
             shutil.copy2(self.source_path / file_name, self.project_path / file_name)
 
-    def rename_ids(self):
-        for dir in self.dirs:
-            dir_path = self.project_path / dir
-            for file_path in dir_path.glob(f"**/{self.source_id}*"):
-                print(f" --> Renaming file : {file_path}...")
-                new_file_path = file_path.with_name(
-                    file_path.name.replace(self.source_id, self.id)
-                )
-                file_path.rename(new_file_path)
+    def rename_files(self, key, new_key):
+        dir_path = self.project_path
+        for file_path in dir_path.glob(f"**/*{key}*"):
+            print(f" --> Renaming file : {file_path}...")
+            new_file_path = file_path.with_name(file_path.name.replace(key, new_key))
+            file_path.rename(new_file_path)
 
     def patch_files(self):
         patch_extensions = [
@@ -116,7 +116,8 @@ class ProjectCreator:
         self.copy_directories()
         self.cleanup_unwanted()
         self.copy_files()
-        self.rename_ids()
+        self.rename_files(self.source_name, self.name)
+        self.rename_files(self.source_id, self.id)
         self.patch_files()
         self.post_actions()
         print(
